694e199119045df60742b1b94c994fc942d3d74d
baby0112u <baby0112u@gmail.com>
2019-12-01 23:19:17 +0800

    init backup

---
+++ SerilogDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace SerilogDemo.Pages {
+    public class IndexModel : PageModel {
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger) {
+            _logger = logger;
+        }
+
+        public void OnGet() {
+            _logger.LogInformation("You requested the Index page.");
+            try {
+                for (int i = 0; i < 100; i++) {
+                    if (i == 56) {
+                        throw new Exception("This is our Exception");
+                    } else {
+                        _logger.LogInformation($"The value of i is {i}");
+                    }
+                }
+            } catch (Exception ex) {
+                _logger.LogError(ex, "We cautch this exception in the Index Get call.");
+                //throw;
+            }
+        }
+    }
+}
